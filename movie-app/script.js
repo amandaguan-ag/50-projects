197e481f8759549ae9da294dcf26144d3342b2ea
amandaguan-ag <amandaguan1314@gmail.com>
2025-03-17 16:14:05 -0400

    Add configuration file and update script to use API key from config, enhancing code organization

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,4 +1,4 @@
-const API_KEY = '3fd2be6f0c70a2a598f084ddfb75487c'
+import { API_KEY } from './config.js'
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query='`
